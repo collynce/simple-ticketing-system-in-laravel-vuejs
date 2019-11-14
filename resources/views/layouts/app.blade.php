13f9d4b640f1dc1d5a1e308c30efa8caa49f094d
Collins Rono <collinsrono02@gmail.com>
2019-11-14 19:46:47 +0300

    Initial Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
     <title>Laugh Industry</title>
 
     <!-- Scripts -->
-    <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+    <script src="{{secure_asset('js/app.js')}}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     
     <!-- Styles -->
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <link href="{{secure_asset('css/app.css')}}" rel="stylesheet">
 </head>
 <style>
 html,
