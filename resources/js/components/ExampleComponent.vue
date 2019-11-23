1b657209ef305f0fb3f09c686967af7fa51718b2
collynce <40454130+collynce@users.noreply.github.com>
2019-11-23 11:03:42 +0300

    Made some changes

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -66,6 +66,24 @@
         <a :href="`/client/`+event.id" class="btn btn-primary">Buy Tickets</a>
       </small>
     </div>
+<nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-bind:class="[{disabled: !pagination.prev_page_url}]" class="page-item">
+          <a class="page-link" href="#" @click="fetchEvents(pagination.prev_page_url)">Previous</a>
+        </li>
+
+        <li class="page-item disabled">
+          <a
+            class="page-link text-dark"
+            href="#"
+          >Page {{ pagination.current_page }} of {{ pagination.last_page }}</a>
+        </li>
+
+        <li v-bind:class="[{disabled: !pagination.next_page_url}]" class="page-item">
+          <a class="page-link" href="#" @click="fetchEvents(pagination.next_page_url)">Next</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
